6f12b98518e6e0c426e180fc14d3d05ebdf9fb29
Artem <artemklymenko892@gmail.com>
2024-07-15 21:35:19 +0300

    Enabled edge-to-edge display and created WeatherScreen in MainActivity

--- app/src/main/java/com/artemklymenko/myweatherapp/MainActivity.kt
+++ app/src/main/java/com/artemklymenko/myweatherapp/MainActivity.kt
@@ -1,24 +1,37 @@
 package com.artemklymenko.myweatherapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.artemklymenko.myweatherapp.ui.screens.WeatherScreen
 import com.artemklymenko.myweatherapp.ui.theme.MyWeatherAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
         setContent {
             MyWeatherAppTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding()
+                        .navigationBarsPadding(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    WeatherScreen()
                 }
             }
         }
