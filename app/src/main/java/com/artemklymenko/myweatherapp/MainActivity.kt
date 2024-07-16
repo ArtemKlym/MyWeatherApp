29e67a96c73f9df07e28586dbfcf5b7f7d451eb3
Artem <artemklymenko892@gmail.com>
2024-07-16 17:17:28 +0300

    Switched from Surface to Scaffold to fix enableEdgeToEdge

--- app/src/main/java/com/artemklymenko/myweatherapp/MainActivity.kt
+++ app/src/main/java/com/artemklymenko/myweatherapp/MainActivity.kt
@@ -6,33 +6,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import com.artemklymenko.myweatherapp.ui.screens.WeatherScreen
 import com.artemklymenko.myweatherapp.ui.theme.MyWeatherAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
             navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
         )
+        super.onCreate(savedInstanceState)
         setContent {
             MyWeatherAppTheme {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .statusBarsPadding()
-                        .navigationBarsPadding(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    WeatherScreen()
-                }
+                WeatherScreen()
             }
         }
     }
--- app/src/main/java/com/artemklymenko/myweatherapp/ui/screens/WeatherScreen.kt
+++ app/src/main/java/com/artemklymenko/myweatherapp/ui/screens/WeatherScreen.kt
@@ -2,19 +2,31 @@ package com.artemklymenko.myweatherapp.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.artemklymenko.myweatherapp.ui.screens.sections.TabLayout
 import com.artemklymenko.myweatherapp.ui.screens.sections.WeatherCard
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun WeatherScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        WeatherCard()
-        TabLayout()
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+                .statusBarsPadding()
+                .navigationBarsPadding()
+        ) {
+            WeatherCard()
+            TabLayout()
+        }
     }
 }
