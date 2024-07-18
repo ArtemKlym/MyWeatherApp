9b5078dce113f3d2cee54066081f738c5337159f
Artem <artemklymenko892@gmail.com>
2024-07-18 14:12:23 +0300

    Implemented WeatherViewModel in WeatherScreen

--- app/src/main/java/com/artemklymenko/myweatherapp/ui/screens/WeatherScreen.kt
+++ app/src/main/java/com/artemklymenko/myweatherapp/ui/screens/WeatherScreen.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.artemklymenko.myweatherapp.ui.screens.sections.TabLayout
 import com.artemklymenko.myweatherapp.ui.screens.sections.WeatherCard
 
@@ -15,14 +17,18 @@ import com.artemklymenko.myweatherapp.ui.screens.sections.WeatherCard
 @Preview(showBackground = true)
 @Composable
 fun WeatherScreen() {
+    val weatherViewModel: WeatherViewModel = hiltViewModel()
+    val weatherData = remember { weatherViewModel.allWeatherData }
+    val currentWeatherData = remember { weatherViewModel.currentWeatherData }
+    weatherViewModel.fetchWeather("London")
     Scaffold {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
         ) {
-            WeatherCard()
-            TabLayout()
+            WeatherCard(currentWeatherData)
+            TabLayout(weatherData.value, currentWeatherData)
         }
     }
 }
